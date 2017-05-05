7d4c0d9aecc1f5d886d531d9c0d2d418ed8b10f3
Paeron <perjaa@kth.se>
2017-05-05 17:31:50 +0200

    Better selecting mechanism

--- js/startCtrl.js
+++ js/startCtrl.js
@@ -1,9 +1,29 @@
 
 mobiluApp.controller('StartCtrl',function($scope,userData){
 
+	$scope.rock = false;
+	$scope.paper = false;
+	$scope.scissors = false;
+
 	$scope.play = function(team) {
-		userData.setTeam(team);
+		var current = userData.getTeam();
+		
+		if (current == "") {
+			userData.setTeam(team);
+
+			if (team == "rock") {
+				$scope.paper = true;
+				$scope.scissors = true;
+			}
+			else if (team == "paper") {
+				$scope.rock = true;
+				$scope.scissors = true;
+			}
+			else if (team == "scissors") {
+				$scope.rock = true;
+				$scope.paper = true;
+			}
+		}	
 		window.location.hash = "#!/play";
 	}
-
 });
