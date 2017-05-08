44b131072f7054e2969ea3b1948e96ad9a2eb6cb
Paeron <perjaa@kth.se>
2017-05-08 16:41:37 +0200

    there is a map there now

--- js/mapCtrl.js
+++ js/mapCtrl.js
@@ -1,7 +1,16 @@
 
-mobiluApp.controller('MapCtrl',function($scope,Firebase,$timeout){
+mobiluApp.controller('MapCtrl',function($scope,Firebase,$timeout,NgMap){
 
 	$scope.text = "Conquer-Info:";
+
+	$scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLTqYGgmbIGdjSQxeRb9JjVQ5Tq4FANdc";
+
+	NgMap.getMap().then(function(map) {
+		console.log(map);
+   		console.log(map.getCenter());
+    	console.log('markers', map.markers);
+    	console.log('shapes', map.shapes);
+  	});
 
 	Firebase.getLocData(function(data) {
 
