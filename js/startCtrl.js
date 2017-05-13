b7a83f51dc4acafe4747ddf3234aa4285f9bf4bf
Paeron <perjaa@kth.se>
2017-05-13 13:56:58 +0200

    login fix

--- js/startCtrl.js
+++ js/startCtrl.js
@@ -1,28 +1,32 @@
 
 mobiluApp.controller('StartCtrl',function($scope,userData){
 
-	$scope.rock = false;
-	$scope.paper = false;
-	$scope.scissors = false;
+	var team = userData.getTeam();
+
+	if (team == '""') {
+		$scope.rock = false;
+		$scope.paper = false;
+		$scope.scissors = false;
+	}
+	else if (team == "rock") {
+		$scope.paper = true;
+		$scope.scissors = true;
+	}
+	else if (team == "paper") {
+		$scope.rock = true;
+		$scope.scissors = true;
+	}
+	else if (team == "scissors") {
+		$scope.rock = true;
+		$scope.paper = true;
+	}
+	
 
 	$scope.play = function(team) {
 		var current = userData.getTeam();
 		
 		if (current == '""') {
 			userData.setTeam(team);
-
-			if (team == "rock") {
-				$scope.paper = true;
-				$scope.scissors = true;
-			}
-			else if (team == "paper") {
-				$scope.rock = true;
-				$scope.scissors = true;
-			}
-			else if (team == "scissors") {
-				$scope.rock = true;
-				$scope.paper = true;
-			}
 		}
 		//hyper.log(window.location)
 		//window.location = "/#!/play";
