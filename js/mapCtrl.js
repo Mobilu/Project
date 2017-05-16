6da6a664b80e1250713044b8f74555d4507854c5
Paeron <perjaa@kth.se>
2017-05-16 11:28:47 +0200

    map fix

--- js/mapCtrl.js
+++ js/mapCtrl.js
@@ -54,24 +54,20 @@ mobiluApp.controller('MapCtrl',function($scope,Firebase,$timeout,$interval,NgMap
 	$scope.conquer = function(place,coords) {
 
 		var distance = 0;
-		distance += (coords[0] - $scope.location[0])*(coords[0] - $scope.location[0]);
-		distance += (coords[1] - $scope.location[1])*(coords[1] - $scope.location[1]);
-		distance = Math.sqrt(distance)*82.5;
-
-		if (distance >= 0.05) {
+		//distance += (coords[0] - $scope.location[0])*(coords[0] - $scope.location[0]);
+		//distance += (coords[1] - $scope.location[1])*(coords[1] - $scope.location[1]);
+		//distance = Math.sqrt(distance)*82.5;
+		distance = getDistanceFromLatLonInKm(coords[0],coords[1],$scope.location[0],$scope.location[1]);
+		if (distance >= 0.03) {
 			console.log("TOO FAR AWAY")
 		}
 		else {
-			if ($scope.place != "") {
+			if (place != "") {
 				Firebase.conquer(place,myTeam);
 				userData.addPlace(place);
 				Firebase.setMyPlaces(userData.getPlacesArray());
 			}
 		}
-	}
-
-
-
 	}
 
 	function onSuccess(position) {
@@ -89,5 +85,23 @@ mobiluApp.controller('MapCtrl',function($scope,Firebase,$timeout,$interval,NgMap
    			navigator.geolocation.getCurrentPosition(onSuccess);
   		}, 1000)
 	}
+
+	function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
 
 });
